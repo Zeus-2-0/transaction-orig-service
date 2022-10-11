f5f36280445f8978d32d76f2f095e5144d0fc1a8
vbalaji215-dev <vbalaji215@outlook.com>
2022-10-11 08:04:46 -0400

    ZEUS-182 tracked the payload

--- src/main/java/com/brihaspathee/zeus/broker/producer/TransactionPublisher.java
+++ src/main/java/com/brihaspathee/zeus/broker/producer/TransactionPublisher.java
@@ -1,11 +1,15 @@
 package com.brihaspathee.zeus.broker.producer;
 
+import com.brihaspathee.zeus.domain.entity.PayloadTracker;
 import com.brihaspathee.zeus.message.MessageMetadata;
 import com.brihaspathee.zeus.message.ZeusMessagePayload;
+import com.brihaspathee.zeus.util.ZeusRandomStringGenerator;
 import com.brihaspathee.zeus.web.model.RawTransactionDto;
+import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.apache.commons.lang3.StringUtils;
 import org.apache.kafka.clients.producer.ProducerRecord;
 import org.apache.kafka.common.header.internals.RecordHeader;
 import org.springframework.kafka.core.KafkaTemplate;
@@ -49,18 +53,20 @@ public class TransactionPublisher {
      * Publish the message to Kafka Topic
      * @param rawTransactionDto
      */
-    public void publishTransaction(RawTransactionDto rawTransactionDto){
+    public void publishTransaction(RawTransactionDto rawTransactionDto) throws JsonProcessingException {
         String[] messageDestinations = {"DATA-TRANSFORM-SERVICE", "TRANSACTION-STORAGE-SERVICE"};
+        String payloadId = ZeusRandomStringGenerator.randomString(15);
         ZeusMessagePayload<RawTransactionDto> messagePayload = ZeusMessagePayload.<RawTransactionDto>builder()
                 .messageMetadata(MessageMetadata.builder()
                         .messageSource("TRANSACTION-ORIG-SERVICE")
                         .messageDestination(messageDestinations)
                         .messageCreationTimestamp(LocalDateTime.now())
                         .build())
+                .payloadId(payloadId)
                 .payload(rawTransactionDto)
                 .build();
         transactionPublisherCallback.setRawTransactionDto(rawTransactionDto);
-        // createPayloadTracker(messagePayload);
+        createPayloadTracker(messagePayload);
         ProducerRecord<String, ZeusMessagePayload<RawTransactionDto>> producerRecord =
                 buildProducerRecord(messagePayload);
         kafkaTemplate.send(producerRecord).addCallback(transactionPublisherCallback);
@@ -80,5 +86,24 @@ public class TransactionPublisher {
                 "test payload id 2",
                 messagePayload,
                 Arrays.asList(messageHeader));
+    }
+
+    /**
+     * Create the payload tracker detail record
+     * @param messagePayload
+     * @throws JsonProcessingException
+     */
+    private void createPayloadTracker(ZeusMessagePayload<RawTransactionDto> messagePayload)
+            throws JsonProcessingException {
+        String payloadAsString = objectMapper.writeValueAsString(messagePayload);
+        PayloadTracker payloadTracker = PayloadTracker.builder()
+                .payloadDirectionTypeCode("OUTBOUND")
+                .payload_key("TRANSACTION")
+                .payload_key_type_code(messagePayload.getPayload().getZtcn())
+                .payload(payloadAsString)
+                .payloadId(messagePayload.getPayloadId())
+                .sourceDestinations(StringUtils.join(
+                        messagePayload.getMessageMetadata().getMessageDestination()))
+                .build();
     }
 }
